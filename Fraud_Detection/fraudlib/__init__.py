45d9f2c63275dc94fb09af2b10ad2938104571d6
Shivani Mittal <85379484+ShivM99@users.noreply.github.com>
2024-09-19 16:43:42 +0530

    Add files via upload

---
+++ Fraud_Detection/fraudlib/__init__.py
@@ -0,0 +1,5 @@
+# Defining the '__all__' variable that indicate the module names that should be imported when using the * operator
+__all__ = ['myfunctions']
+
+# Importing the modules
+from . import myfunctions
